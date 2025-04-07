e2ce7599652fa5339326c5060a6ebbee0b7c8033
Mathan <105858419+Mathan48@users.noreply.github.com>
2025-04-07 16:25:14 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -15,7 +15,7 @@ resource "aws_instance" "web" {
   connection {
     type        = "ssh"
     user        = "ec2-user"
-    private_key = file("last.pem")
+    private_key = file("./last.pem")
     host        = self.public_ip
   }
   provisioner "remote-exec" {
