11107028c522a511223c1d761a48b5b22ec2765b
Mathan <105858419+Mathan48@users.noreply.github.com>
2025-04-07 15:57:22 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,65 @@
+resource "aws_key_pair" "deployer" {
+  key_name   = "deployer-key"
+  public_key = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIClX37V4hmEPNC994+9b5HZUQG6Nvo4YqLy1Rvp4dHDF mathan.thirumugam@Asplap3226"
+}
+
+
+resource "aws_instance" "web" {
+  ami                    = "ami-00a929b66ed6e0de6"
+  instance_type          = "t2.medium"
+  key_name               = aws_key_pair.deployer.key_name
+  vpc_security_group_ids = ["${aws_security_group.allow_tls.id}"]
+  tags = {
+    Name = "web server"
+  }
+  connection {
+    type        = "ssh"
+    user        = "ec2-user"
+    private_key = file("last.pem")
+    host        = self.public_ip
+  }
+  provisioner "remote-exec" {
+    inline = [
+      "sudo yum update -y",
+      "sudo yum install -y httpd.x86_64",
+      "sudo systemctl start httpd.service",
+      "sudo systemctl enable httpd.service"
+
+    ]
+  }
+}
+
+resource "aws_security_group" "allow_tls" {
+  name        = "allow_tls"
+  description = "Allow TLS inbound traffic"
+
+  ingress {
+    description = "TLS from VPC"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    description = "TLS from VPC"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "allow_tls"
+  }
+}
+
+output "ip" {
+  value = aws_instance.web.public_ip
+}
+
